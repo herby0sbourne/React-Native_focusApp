a5af765e287fad03c25bbe9b4ec752e4f465c237
Herby <Herbyosbourne31@gmail.com>
2024-02-02 13:03:55 -0500

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { Platform, StyleSheet, Text, View } from "react-native";
+
+import Focus from "./src/features/focus/Focus";
+import Timer from "./src/features/timer/Timer";
+
+import { colors } from "./src/utils/colors";
+import { spacing } from "./src/utils/sizes";
 
 export default function App() {
+  const [focusSubject, setFocusSubject] = useState("grading");
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style="light" />
+      <View style={styles.container}>
+        {focusSubject ? (
+          <Timer focusSubject={focusSubject} />
+        ) : (
+          <Focus addSubject={setFocusSubject} />
+        )}
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: colors.bgPrimary,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: Platform.select({ android: spacing.md, ios: spacing.lg }),
   },
 });
